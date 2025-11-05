89cc1959285a03e82fc7035951bf02a17573ab79
GeniusCodes007 <geniuscodes007@gmail.com>
2025-11-05 22:30:27 +0100

    Data Analysis

--- practice.py
+++ practice.py
@@ -1,5 +1,17 @@
 import pandas as pnd
 
+t_e = []
+for x in range(0, 398):
+    #print(x+1)
+    t_e.append(x+1)
 
+#print(tup_le)
 bn = pnd.read_csv('Bitcoin_Prices_List.csv')
+bn.index = t_e
 print(bn)
+#print(bn.index)
+
+"""
+with open('Bitcoin_Prices_List.csv', 'r') as prices:
+    for index, lines in prices:
+        print(index + 1, lines.read_lines())"""
